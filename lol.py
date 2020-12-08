9e59aebcac55cca43ee23085ed215a68a0d11aea
wur3 <wu.richard.98@gmail.com>
2020-12-07 22:45:52 -0500

    option to remove spaces

--- lol.py
+++ lol.py
@@ -3,16 +3,24 @@ import requests
 import json
 
 def main():
+    noSpaces = ''
+    while(noSpaces != 'y' and noSpaces != 'n'):
+        noSpaces = input('Do you want the words with spaces?(y/n)\n').strip().lower()
+    if noSpaces == 'y':
+        noSpaces = True
+    elif noSpaces == 'n':
+        noSpaces = False
+
     response = requests.get('http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json')
     if response:
         json_res = json.loads(response.content)
         champions = [champ['name'] for champ in json_res['data'].values()]
-        print(champions)
         f = open('lol_champion_names.txt', 'w')
-        for champ in champions:
-            f.write(champ)
-            f.write(', ')
+        formatted = ',\n'.join(champions)
+        if noSpaces:
+            formatted = formatted.replace(' ','')
         print("Tailyah")
+        f.write(formatted)
     else:
         print('API call failed')
 
