e2ea11e168a0bcb57f8b74db4e84797c5bc7aa98
wur3 <wu.richard.98@gmail.com>
2021-02-02 11:30:57 -0500

    uses newest patch number

--- lol.py
+++ lol.py
@@ -14,7 +14,16 @@ def main():
             break
         else:
             noSpaces = input('Answer with \'y\' or \'n\':\n')
-    response = requests.get('http://ddragon.leagueoflegends.com/cdn/11.2.1/data/en_US/champion.json')
+    # get the current patch number for League of Legends
+    response = requests.get('https://ddragon.leagueoflegends.com/api/versions.json')
+    if response:
+        json_res = json.loads(response.content)
+        current_ver = json_res[0]
+    else:
+        print('API call for game patch number failed')
+
+    # get the list of all champions
+    response = requests.get('http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/champion.json'.format(current_ver))
     if response:
         json_res = json.loads(response.content)
         champions = [champ['name'] for champ in json_res['data'].values()]
@@ -22,10 +31,10 @@ def main():
         formatted = ',\n'.join(champions)
         if noSpaces:
             formatted = formatted.replace(' ','')
-        print("Tailyah")
+        print("Check lol_chapmion_names.txt")
         f.write(formatted)
     else:
-        print('API call failed')
+        print('API call for champions failed')
 
 if __name__ == '__main__':
     main()
